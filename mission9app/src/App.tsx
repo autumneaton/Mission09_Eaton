31192c7fa474b1c397d8ef2992ccaca81fc8e88d
Autumn Eaton <aeaton8@byu.edu>
2024-03-08 17:20:00 -0700

    Finished assignment

--- mission9app/src/App.tsx
+++ mission9app/src/App.tsx
@@ -1,24 +1,48 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import CollegeBasketballTeams from "./CollegeBasketballTeams.json";
+
+interface Team {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Heading() {
+  return <h1>Colleges in NCAA Basketball</h1>;
+}
+
+class College extends React.Component<Team> {
+  render() {
+    const oneCollege = this.props;
+
+    return (
+      <div className="card">
+        <h2>School Name: {oneCollege.school}</h2>
+        <h2>Mascot Name: {oneCollege.name}</h2>
+        <h2>City: {oneCollege.city}</h2>
+        <h2>State: {oneCollege.state}</h2>
+      </div>
+    );
+  }
+}
+
+function CollegeList() {
+  return (
+    <div className="card-container">
+      {CollegeBasketballTeams.teams.map((team: Team) => (
+        <College key={team.school} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <CollegeList />
     </div>
   );
 }
